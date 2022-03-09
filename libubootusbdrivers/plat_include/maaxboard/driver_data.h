f01a81c41fefe8778bccc959dad5c4904fc7e7b7
stephen <stephen.gw@gmail.com>
2022-03-09 12:26:39 +0000

    Improved linker_lists version

--- libubootusbdrivers/plat_include/maaxboard/driver_data.h
+++ libubootusbdrivers/plat_include/maaxboard/driver_data.h
@@ -9,31 +9,20 @@
 #include <usb.h>
 
 struct driver_data_t {
-    struct driver driver_list[1];
-	struct uclass_driver uclass_driver_list[2];
+    struct driver driver_array[1];
+	struct uclass_driver uclass_driver_array[3];
+    struct usb_driver_entry usb_driver_entry_array[0];
+    struct driver_info driver_info_array[0];
+    struct udevice udevice_array[0];
 };
 
 extern struct driver_data_t driver_data;
 
-extern struct driver _u_boot_list_2_driver_1;
-extern struct driver _u_boot_list_2_driver_3;
-
-extern struct uclass_driver _u_boot_list_2_uclass_driver_1;
-extern struct uclass_driver _u_boot_list_2_uclass_driver_3;
-
-extern struct driver_info _u_boot_list_2_driver_info_1;
-extern struct driver_info _u_boot_list_2_driver_info_3;
-
-extern struct udevice _u_boot_list_2_udevice_1;
-extern struct udevice _u_boot_list_2_udevice_3;
-
-extern struct usb_driver_entry _u_boot_list_2_usb_driver_entry_1;
-extern struct usb_driver_entry _u_boot_list_2_usb_driver_entry_3;
-
 extern struct driver _u_boot_list_2_driver_2_root_driver;
 extern struct uclass_driver _u_boot_list_2_uclass_driver_2_root;
 extern struct uclass_driver _u_boot_list_2_uclass_driver_2_usb;
-extern struct udevice _u_boot_list_2_udevice_2_root;
+extern struct uclass_driver _u_boot_list_2_uclass_driver_2_usb_hub;
+extern struct udevice _u_boot_list_2_udevice_2_root; // Not defined...
 
 void initialise_driver_data(void);
 
--- libubootusbdrivers/src/plat/maaxboard/driver_data.c
+++ libubootusbdrivers/src/plat/maaxboard/driver_data.c
@@ -9,30 +9,12 @@
 
 struct driver_data_t driver_data;
 
-struct driver _u_boot_list_2_driver_1;
-struct driver _u_boot_list_2_driver_3;
-
-struct uclass_driver _u_boot_list_2_uclass_driver_1;
-struct uclass_driver _u_boot_list_2_uclass_driver_3;
-
-struct driver_info _u_boot_list_2_driver_info_1; // Not defined...
-struct driver_info _u_boot_list_2_driver_info_3; // Not defined...
-
-struct udevice _u_boot_list_2_udevice_1; // Not defined...
-struct udevice _u_boot_list_2_udevice_3; // Not defined...
-
-struct usb_driver_entry _u_boot_list_2_usb_driver_entry_1; // Not defined...
-struct usb_driver_entry _u_boot_list_2_usb_driver_entry_3; // Not defined...
-
 struct udevice _u_boot_list_2_udevice_2_root; // Not defined...
 
 void initialise_driver_data(void) {
-    driver_data.driver_list[0] = _u_boot_list_2_driver_2_root_driver;
-    _u_boot_list_2_driver_1 = driver_data.driver_list[0];
-    _u_boot_list_2_driver_3 = driver_data.driver_list[0];
+    driver_data.driver_array[0] = _u_boot_list_2_driver_2_root_driver;
 
-    driver_data.uclass_driver_list[0] = _u_boot_list_2_uclass_driver_2_root;
-    driver_data.uclass_driver_list[1] = _u_boot_list_2_uclass_driver_2_usb;
-    _u_boot_list_2_uclass_driver_1 = driver_data.uclass_driver_list[0];
-    _u_boot_list_2_uclass_driver_3 = driver_data.uclass_driver_list[1];
+    driver_data.uclass_driver_array[0] = _u_boot_list_2_uclass_driver_2_root;
+    driver_data.uclass_driver_array[1] = _u_boot_list_2_uclass_driver_2_usb;
+    driver_data.uclass_driver_array[2] = _u_boot_list_2_uclass_driver_2_usb_hub;
 }
