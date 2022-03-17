db2df561429c18d4c054f6584ba2e8a085fb7c31
stephen <stephen.gw@gmail.com>
2022-03-17 23:15:37 +0000

    Add explanatory text

--- libubootusbdrivers/include/driver_data.h
+++ libubootusbdrivers/include/driver_data.h
@@ -11,8 +11,16 @@
 #include <plat_driver_data.h>
 
 struct driver_data_t {
+    /* The array of drivers compiled in */
     struct driver driver_array[DEFINE_UBOOT_DRIVER_COUNT];
+    /* The array of driver classes compiled in */
 	struct uclass_driver uclass_driver_array[DEFINE_UBOOT_UCLASS_DRIVER_COUNT];
+    /*
+     * The remaining entries are used for 'static' drivers, i.e. drivers to
+     * be statically bound to devices. These are normally empty / unused;
+     * drivers are normally bound to devices at run-time based upon the
+     * contents of the device tree and the compatibility of the driver.
+     */
     struct usb_driver_entry usb_driver_entry_array[DEFINE_UBOOT_USB_DRIVER_ENTRY_COUNT];
     struct driver_info driver_info_array[DEFINE_UBOOT_DRIVER_INFO_COUNT];
     struct udevice udevice_array[DEFINE_UBOOT_UDEVICE_COUNT];
