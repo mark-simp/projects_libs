dfb23ff9f9c447803d2a3e25966af374169471ee
stephen <stephen.gw@gmail.com>
2022-03-30 22:30:28 +0100

    PUll in blk drivers and simplify config

--- libubootusbdrivers/plat_include/maaxboard/uboot_helper.h
+++ libubootusbdrivers/plat_include/maaxboard/uboot_helper.h
@@ -18,19 +18,15 @@
 #define CONFIG_DM                       1
 #define CONFIG_DM_USB                   1
 #define CONFIG_DM_DEVICE_REMOVE			1
-#define CONFIG_DM_RESET         		0 /* Don't need to handle reset */
+
+#define CONFIG_CMDLINE                  1
+#define CONFIG_SILENT_CONSOLE           1
 
 #define CONFIG_OF_LIBFDT				1
 #define CONFIG_OF_LIVE   				1
 #define CONFIG_OF_REAL          		1
 #define CONFIG_OF_CONTROL          		1
-#define CONFIG_OF_PLATDATA				0
-#define CONFIG_OF_PLATDATA_RT   		0
-#define CONFIG_OF_PLATDATA_DRIVER_RT	0
-#define CONFIG_OF_PLATDATA_INST			0
-#define CONFIG_OF_PLATDATA_NO_BIND		0
-#define CONFIG_OF_PLATDATA_PARENT		0
-#define CONFIG_OF_TRANSLATE	            0
+
 #define CONFIG_SIMPLE_BUS_CORRECT_RANGE 1
 #define CONFIG_PHY                      1
 #define CONFIG_USB_XHCI_DWC3			1
@@ -38,33 +34,27 @@
 #define CONFIG_USB_STORAGE              1
 #define CONFIG_USB_HOST                 1
 #define CONFIG_USB_XHCI_HCD             1
-#define CONFIG_USB_DWC3_GADGET          0
 #define CONFIG_DEVRES           		1
-#define CONFIG_DM_DMA           		0
-#define CONFIG_PHYS_TO_BUS      		0
-#define CONFIG_DM_SEQ_ALIAS				0
-#define CONFIG_POWER_DOMAIN				0 /* No power domain driver support */
-#define CONFIG_IOMMU					0 /* No IOMMU driver support */
-#define CONFIG_CLK    					0 /* No clock driver support */
-#define CONFIG_PCI						0 /* No PCI support */
-#define CONFIG_NEEDS_MANUAL_RELOC   	0
-#define CONFIG_SIMPLE_BUS				0
-#define CONFIG_LOG						0
-#define CONFIG_OF_SEPARATE              0
-#define CONFIG_OF_BOARD                 0
-#define CONFIG_MULTI_DTB_FIT            0
-#define CONFIG_SPL_SEPARATE_BSS         0
-#define CONFIG_PHANDLE_CHECK_SEQ        0
+
+#define CONFIG_BLK                      1
 
 #define CONFIG_LOGLEVEL				0
 #define CONFIG_ERR_PTR_OFFSET   	0
 #define CONFIG_NR_DRAM_BANKS		0
 #define CONFIG_SYS_CACHELINE_SIZE 	64 /* for cortex a53 MPCore */
+#define CONFIG_SYS_MAXARGS          64
+#define CONFIG_SYS_CBSIZE           256
+#define CONFIG_SYS_PBSIZE           256
+#define CONFIG_SYS_PROMPT           ""
+#define CONFIG_ENV_SIZE             0x2000
 #define ARCH_DMA_MINALIGN   		CONFIG_SYS_CACHELINE_SIZE
-#define DO_DEPS_ONLY				true
+// #define DO_DEPS_ONLY				true
 #define errno_str(X)				""
 #define CONFIG_SYS_HZ               1000
-#define MAX_FILES                   3
+#define UBOOT_STDIN		            0
+#define UBOOT_STDOUT		        1
+#define UBOOT_STDERR		        2
+#define MAX_FILES	                3
 
 #define mutex_init(...)
 #define mutex_lock(...)
@@ -77,11 +67,11 @@
 
 #define _DEBUG			false
 
+#include <linux/kconfig.h>
+#include <common.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdio.h>
 #include <string.h>
-#include <linux/kconfig.h>
 #include <vsprintf.h>
 #include <assert.h>
 #include <time.h>
@@ -98,3 +88,4 @@
 #include <asm/io.h>
 #include <driver_data.h>
 #include <command.h>
+#include <exports.h>
