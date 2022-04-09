86f9193ab8cd9ff17b76b854e39daaa3ccc639e7
stephen <stephen.gw@gmail.com>
2022-04-09 20:04:26 +0100

    Tidy up errno

---
+++ libubootdrivers/include/wrapper/errno.h
@@ -0,0 +1,15 @@
+/* Minimal stub replacement of 'errno' */
+#ifndef _ERRNO_H
+#define _ERRNO_H
+
+#include <linux/errno.h>
+
+extern _Thread_local int errno;
+
+#define __set_errno(val) do { errno = val; } while (0)
+
+static const char error_message[] = "";
+
+static inline const char *errno_str(int errno) { return error_message; }
+
+#endif /* _ERRNO_H */
