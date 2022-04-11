e5160bbe24c538d69ef67f2f22b238b4f0c3409f
stephen <stephen.gw@gmail.com>
2022-04-11 12:28:12 +0100

    Correct panic stub

--- libubootdrivers/src/wrapper/panic.c
+++ libubootdrivers/src/wrapper/panic.c
@@ -1,14 +1,13 @@
 /* Provide an seL4 compliant implemention of panic as used by U-Boot */
 
-#include <stdio.h>
-#include <stdarg.h>
+#include <uboot_helper.h>
 #include <hang.h>
 
 void panic(const char *fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	vprintf(fmt, args);
+	printf(fmt, args);
 	va_end(args);
 	hang();
 }
